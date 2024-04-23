dfafcc4637441c020096095e668b4942f2722047
tomellm <thomas.ellmenreich@gmail.com>
2024-04-23 11:03:13 +0200

    #46 externalised organisation
    the organisation can now be configured in the
    .env file and then used through the
    class

--- github-adapter/src/main/java/be/xplore/githubmetrics/githubadapter/WorkFlowRunsAdapter.java
+++ github-adapter/src/main/java/be/xplore/githubmetrics/githubadapter/WorkFlowRunsAdapter.java
@@ -2,11 +2,13 @@ package be.xplore.githubmetrics.githubadapter;
 
 import be.xplore.githubmetrics.domain.domain.WorkflowRun;
 import be.xplore.githubmetrics.domain.usecases.ports.in.WorkflowRunsQueryPort;
+import be.xplore.githubmetrics.githubadapter.config.GithubConfig;
 import be.xplore.githubmetrics.githubadapter.mappingclasses.GHActionRuns;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.List;
 
@@ -15,17 +17,23 @@ public class WorkFlowRunsAdapter implements WorkflowRunsQueryPort {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WorkFlowRunsAdapter.class);
 
+    private final GithubConfig config;
     private final GithubAdapter githubAdapter;
 
-    public WorkFlowRunsAdapter(GithubAdapter githubAdapter) {
+    public WorkFlowRunsAdapter(GithubConfig config, GithubAdapter githubAdapter) {
+        this.config = config;
         this.githubAdapter = githubAdapter;
     }
 
     @Override
     public List<WorkflowRun> getLastDaysWorkflows() {
-        var parameterMap = new HashMap<String, String>();
-        parameterMap.put("created", ">=2024-04-10");
-        var responseSpec = githubAdapter.getResponseSpec("/repos/github-insights/github-metrics/actions/runs", parameterMap);
+        var responseSpec = githubAdapter.getResponseSpec(
+                MessageFormat.format(
+                        "/repos/{0}/github-metrics/actions/runs",
+                        this.config.org()
+                ),
+                new HashMap<String, String>()
+        );
         GHActionRuns actionRuns = responseSpec.body(GHActionRuns.class);
         if (actionRuns == null) {
             throw new RuntimeException("Unexpected error in parsing Workflow Runs");
