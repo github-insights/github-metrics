1d3266aa3c405e5de40c8367403c2da62dcbf41b
tomellm <thomas.ellmenreich@gmail.com>
2024-05-07 10:53:35 +0200

    #80 switched to virtual threads
    Before the MetricScheduler would have spring inject a TaskScheduler
    which did not use Virtual threads.
    
    Now we provide our own SimpleAsyncTaskScheduler which is set to use
    Virtual threads. I have confirmed this by logging the current thread
    during a scheduled run.

--- prometheus-exporter/src/main/java/be/xplore/githubmetrics/prometheusexporter/config/SchedulingConfiguration.java
+++ prometheus-exporter/src/main/java/be/xplore/githubmetrics/prometheusexporter/config/SchedulingConfiguration.java
@@ -1,8 +1,11 @@
 package be.xplore.githubmetrics.prometheusexporter.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.SimpleAsyncTaskScheduler;
 
 @ConditionalOnProperty(
         value = "app.scheduling.enable", havingValue = "true", matchIfMissing = true
@@ -10,4 +13,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @EnableScheduling
 public class SchedulingConfiguration {
+
+    @Bean
+    TaskScheduler taskScheduler() {
+        var scheduler = new SimpleAsyncTaskScheduler();
+        scheduler.setVirtualThreads(true);
+        return scheduler;
+    }
 }
