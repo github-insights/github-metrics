9cd58d185efaef30b459070c66146273e6493daa
Gerd Goemans <gerd.goemans@xplore.be>
2024-05-21 08:35:51 +0200

    Added debug param to sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,6 +105,7 @@ sonar {
             "sonar.coverage.jacoco.xmlReportPaths",
             "build/reports/jacoco/testCodeCoverageReport/testCodeCoverageReport.xml"
         )
+        property("sonar.log.level", "DEBUG")
         //property("sonar.qualitygate.wait", "true")
     }
 }
